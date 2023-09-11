d90396ed815bfb59673dbeff64b5805c9ea75344
mahsa <kiaradmahsa@gmail.com>
2023-09-11 08:43:53 +0100

    create blog section template + create article component template + add their Css

--- src/components/Article/article.jsx
+++ src/components/Article/article.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './article.css';
-const Article= () =>{
+const Article= ({imgUrl, date, title}) =>{
     return(
-        <div>
-            <h1>Article</h1>
+        <div className="gpt3__blog-container_article">
+            <div className="gpt3__blog-container_article-image">
+                <img src={imgUrl} alt="blog image"/>
+            </div>
+            <div className="gpt3__blog-container_article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <a href="#">Read full Article</a>
+            </div>
         </div>
     )
 }
 
-export default Article
+export default Article;
